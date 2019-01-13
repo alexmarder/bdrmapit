a4e87f6245e553c04f3a476b9b6e3fb1ca63bd50
Alex Marder <amarder89@gmail.com>
2019-01-13 10:57:50 -0500

    fix

---
+++ bdrmapit_parser/graph/construct3.pyx
@@ -0,0 +1,73 @@
+from traceutils.as2org.as2org cimport AS2Org
+from traceutils.radix.ip2as cimport IP2AS
+
+from bdrmapit_parser.graph.node cimport Interface, Router
+
+
+cdef class Graph:
+    def __init__(self, dict interfaces=None, dict routers=None):
+        if interfaces is None:
+            self.interfaces = {}
+        else:
+            self.interfaces = interfaces
+        if routers is None:
+            self.routers = {}
+        else:
+            self.routers = routers
+
+
+cpdef Graph construct_graph(list addrs, dict nexthop, dict multi, dict dps, IP2AS ip2as, AS2Org as2org, str nodes_file=None):
+    cdef dict interfaces = {}, routers = {}
+    cdef str addr, edge
+    cdef int asn, i
+    cdef Interface interface, succ
+    cdef Router router
+    cdef list edges, dests
+    cdef set origins
+
+    for i in range(len(addrs)):
+        addr = addrs[i]
+        asn = ip2as.asn(addr)
+        interface = Interface(addr, asn, as2org[asn])
+        interfaces[addr] = interface
+    for interface in interfaces.values():
+        if not interface.router:
+            router = Router(interface.addr)
+            interface.router = router
+            router.interfaces.append(interface)
+            routers[router.name] = router
+    for addr, edges in nexthop.items():
+        interface = interfaces[addr]
+        router = interface.router
+        router.nexthop = True
+        for i in range(len(edges)):
+            edge = edges[i]
+            succ = interfaces[edge]
+            if succ in router.succ:
+                router.origins[succ].add(interface.asn)
+            else:
+                router.succ.add(succ)
+                router.origins[succ] = {interface.asn}
+            # router.succ.add(succ)
+            # origins = router.origins.get(succ)
+            # if origins:
+            #     origins.add(interface.asn)
+            # else:
+            #     router.origins[succ] = {interface.asn}
+            succ.pred[router] = succ.pred.get(router, 0) + 1
+    for addr in multi:
+        interface = interfaces[addr]
+        router = interface.router
+        if not router.nexthop:
+            edges = multi[addr]
+            succ = interfaces[edge]
+            router.succ.add(succ)
+            origins = router.origins.get(succ)
+            if origins:
+                origins.add(interface.asn)
+            else:
+                router.origins[succ] = {interface.asn}
+    for addr, dests in dps.items():
+        interface = interfaces[addr]
+        interface.dests.update(dests)
+    return Graph(interfaces=interfaces, routers=routers)
